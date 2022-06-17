02f705135bedc94957f0e28f5468db6860c1f4d8
Alperen Öztaş <alprnoztas@gmail.com>
2022-06-17 19:01:42 +0300

    Homepage has designed.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo List</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body class="bg-info">
+    <div class="container w-25 mt-5">
+        <div class="card shadow-sm">
+            <div class="card-body">
+                <h3>To-do List</h3>
+                <form action="{{route('store')}}" method="post">
+                    @csrf
+                    <div class="input-group">
+                        <input type="text" name="content" class="form-control" placeholder="Add your new todo">
+                        <button type="submit" class="btn btn-dark btn-sm px-4"><i class="fst-italic"></i></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
