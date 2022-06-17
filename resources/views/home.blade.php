343b8ad0d01c9c5f82ecceb68123d863dcac15b1
Alperen Öztaş <alprnoztas@gmail.com>
2022-06-17 19:25:41 +0300

    Todo project has finished with basic concept of to-do list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,14 +13,38 @@
         <div class="card shadow-sm">
             <div class="card-body">
                 <h3>To-do List</h3>
-                <form action="{{route('store')}}" method="post">
+                <form action="{{route('store')}}" method="post" autocomplete="off">
                     @csrf
                     <div class="input-group">
                         <input type="text" name="content" class="form-control" placeholder="Add your new todo">
-                        <button type="submit" class="btn btn-dark btn-sm px-4"><i class="fst-italic"></i></button>
+                        <button type="submit" class="btn btn-dark btn-sm px-4"><i class="fst-italic">ADD</i></button>
                     </div>
                 </form>
+                {{-- if tasks exist count --}}
+                @if (count($todolists))
+                <ul class="list-group list-group-flush mt-3">
+                    @foreach ( $todolists as $todolist)
+                        <li class="list-group-item">
+                            <form action="{{route('destroy',$todolist->id)}}" method="post">
+                                {{ $todolist->content }}
+                                @csrf
+                                @method('delete')
+                                <button type="submit" class="btn btn-link btn-sm float-end">
+                                    <i class="fst-italic">DELETE</i>
+                                </button>
+                            </form>
+                        </li>
+                    @endforeach
+                </ul>
+                @else
+                <p class="text-cente mt-3">No Tasks!</p>
+                @endif
             </div>
+            @if(count($todolists))
+                <div class="card-footer">
+                    You have {{ count($todolists) }} pending tasks!
+                </div>
+            @endif
         </div>
     </div>
 </body>
